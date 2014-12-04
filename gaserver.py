b9078bdef29d6ef3c47814b39450139a79074a31
Andhieka Putra <andhieka.putra@gmail.com>
2014-12-04 18:24:37 +0800

    Created Song, Skill model in database.models.core

--- gaserver.py
+++ gaserver.py
@@ -1,16 +1,22 @@
 from flask import Flask
-#from flask.ext.sqlalchemy import SQLAlchemy
 from database import db
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////gaserver.db'
+
+# attach app to gaserver.database.db
 db.init_app(app)
+# create tables in database
 db.create_all(app=app)
 
 @app.route("/")
 def hello():
     db.create_all()
     return "Hello world!"
+
+@app.route("/user")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
