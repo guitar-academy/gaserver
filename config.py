dac21a29b23eded755f52c2b2002de613fe9abbc
Andhieka Putra <andhieka.putra@gmail.com>
2014-12-08 16:40:08 +0700

    Update database url

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'gaserver.db')
+if os.environ.get('DATABASE_URL') is None:
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'gaserver.db')
+else:
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 WTF_CSRF_ENABLED = True
