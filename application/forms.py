55966cc6996f3a1af0dc1ea8a9944b4431ed6365
Eugene <yeo.eugene.oey@gmail.com>
2014-12-18 06:03:36 +0800

    Clear Flask Project

--- application/forms.py
+++
@@ -1,9 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class ComposeForm(Form):
-    title = StringField('title', validators=[DataRequired()])
-    description = StringField('description', validators=[])
-    notation = StringField('notation', validators=[])
-
--- application/util.py
+++
@@ -1,7 +0,0 @@
-# Utility functions
-def form_to_dictionary(form):
-    json = { }
-    for fieldname, value in form.data.items():
-        json[fieldname] = value
-    return json
-
