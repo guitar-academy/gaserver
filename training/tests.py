b0c329cc891ee532da21afbaa8530dce50a5ef87
Eugene <yeo.eugene.oey@gmail.com>
2014-12-18 22:59:37 +0800

    Add Basic Test

--- training/tests.py
+++ training/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class SongAPITestCase(APITestCase):
+    def test_add_song(self):
+        """
+        Ensure we can add a new song
+        """
+        url = reverse('training:song-list')
+        data = {'title': 'A', 'description': 'Song A', 'notation': 'A1-B2'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        for k, v in data.items():
+            self.assertEqual(response.data[k], v)
+
+    def test_get_song_list(self):
+        """
+        Ensure we can get song list
+        """
+        url = reverse('training:song-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class WarmUpAPITestCase(APITestCase):
+    def test_get_song_list(self):
+        """
+        Ensure we can get warmup list
+        """
+        url = reverse('training:warmup-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
