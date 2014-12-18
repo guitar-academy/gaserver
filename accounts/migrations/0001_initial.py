ce681f35285794758c12e76cec7c7ada916a01df
Eugene <yeo.eugene.oey@gmail.com>
2014-12-18 19:01:51 +0800

    Site data

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -6,15 +6,19 @@ from django.core.management import call_command
 
 def load_data(apps, schema_editor):
     call_command('loaddata', 'user_data')
+    call_command('loaddata', 'site_data')
 
 def unload_data(apps, schema_editor):
     User = apps.get_model('auth', 'User')
+    Site = apps.get_model('sites', 'Site')
     User.objects.all().delete()
+    Site.objects.all().delete()
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '0001_initial'),
+        ('sites', '0001_initial'),
     ]
 
     operations = [
