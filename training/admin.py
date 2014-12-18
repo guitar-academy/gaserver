e05164906798dddfaa14d3753a3f11d57440ae92
Eugene <yeo.eugene.oey@gmail.com>
2014-12-18 16:51:40 +0800

    Training Admin page

--- training/admin.py
+++ training/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Song, Skill, WarmUp, SongSkill, WarmUpSong
+
+admin.site.register(Song)
+admin.site.register(Skill)
+admin.site.register(WarmUp)
+admin.site.register(SongSkill)
+admin.site.register(WarmUpSong)
