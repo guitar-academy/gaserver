16d67711d6d41ed30ea7963e236c2b2399dfd421
Eugene <yeo.eugene.oey@gmail.com>
2014-12-20 03:49:46 +0800

    setup API Authentication Scheme
    - [x] TokenAuthentication
    - [x] SessionAuthentication

--- ga/settings.py
+++ ga/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     # Third Party Applications
     'django_extensions',
     'rest_framework',
+    'rest_framework.authtoken',
 
     # Guitar Academy Applications
     'accounts',
@@ -120,6 +121,10 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'ga.renderers.GABrowsableAPIRenderer',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 # Fixture Directory (Initial Data, Dump Data, Test Data, etc.)
--- ga/urls.py
+++ ga/urls.py
@@ -8,4 +8,12 @@ urlpatterns = patterns('',
     url(r'^$', api_root, name='api-root'),
     url(r'training/',
         include('training.urls', namespace='training', app_name='training')),
+
+    # TokenAuthentication
+    url(r'^token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
+
+    # SessionAuthentication
+    url(r'^session-auth/',
+        include('rest_framework.urls', namespace='rest_framework')),
+
 )
